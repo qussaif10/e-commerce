4d5188925c8aac4557f5f2291c54a69e78d2fc66
qussaif100 <108604744+qussaif10@users.noreply.github.com>
2023-09-26 15:38:16 +0300

    updated navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import websiteLogo from './Logo.png';
+import accountIcon from './accountIcon.png';
+import cartIcon from './cartIcon.png'; // Import the cart icon
 
-function App() {
-  return (
-      <div className="App">
-        <header className="App-header">
-        <p>This is some text.</p>
-        </header>
-      </div>
-  );
-}
+const Navbar = () => {
+    const [searchTerm, setSearchTerm] = useState('');
 
-export default App;
+    const handleSearchChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    return (
+        <nav className="navbar">
+            <img src={websiteLogo} alt="Website Logo" className="website-logo"/>
+            <div className="search-container">
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    value={searchTerm}
+                    onChange={handleSearchChange}
+                />
+                <button>Search</button>
+            </div>
+            <div className="icons-on-the-right">
+            <img src={cartIcon} alt="Shopping Cart" className="cart-icon"/>
+            <img src={accountIcon} alt="Account" className="account-icon"/>
+            </div>
+        </nav>
+    );
+
+};
+
+export default Navbar;
